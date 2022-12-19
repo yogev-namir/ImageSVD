999a8ccad1ac28453b75594a855fe8eb93e79fc0
yogev-namir <yogevnamir97@gmail.com>
2022-12-19 18:50:15 +0200

    works on gray

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
-im
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+import pylab
+from PIL import Image
 
+def low_rank_approx(SVD=None, A=None, r=1):
+    """
+    Computes an r-rank approximation of a matrix
+    given the component u, s, and v of it's SVD
+    Requires: numpy
+    """
+    if not SVD:
+        SVD = np.linalg.svd(A, full_matrices=False)
+    u, s, v = SVD
+    Ar = np.zeros((u.shape[0], v.shape[1]))
+    for i in range(r):
+        Ar += s[i] * np.outer(u.T[i], v[i])
+    return Ar
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+if __name__ == "__main__":
+    """
+    Test: visualize an r-rank approximation of `lena`
+    for increasing values of r
+    Requires: scipy, matplotlib
+    """
 
+    cat = Image.open(f'cat.jpg')
+    gray_cat = cat.convert("L")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    u, s, v = np.linalg.svd(gray_cat, full_matrices=False)
+    i = 1
+    y = low_rank_approx((u, s, v), r=20)
+    Image.fromarray(y.astype('uint8')).show()
